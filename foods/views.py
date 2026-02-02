9f969dd137c267463615e9a29ba5c74ae569c872
Adrien Bangma <adrien.bangma@gmail.com>
2026-02-02 13:45:03 +0100

    first endpoints

--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import FoodItem
+from .serializers import FoodItemSerializer
+
+
+class FoodItemViewSet(viewsets.ModelViewSet):
+    queryset = FoodItem.objects.all().order_by("id")
+    serializer_class = FoodItemSerializer
