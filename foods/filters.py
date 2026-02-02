72f78ebdcd726060f6f43e3b9a3e7b6c1d257e47
Adrien Bangma <adrien.bangma@gmail.com>
2026-02-02 13:46:59 +0100

    add filters search and pagination

---
+++ foods/filters.py
@@ -0,0 +1,28 @@
+from django_filters import rest_framework as filters
+
+from .models import FoodItem
+
+
+class FoodItemFilter(filters.FilterSet):
+    kcal_min = filters.NumberFilter(field_name="kcal_100g", lookup_expr="gte")
+    kcal_max = filters.NumberFilter(field_name="kcal_100g", lookup_expr="lte")
+
+    protein_min = filters.NumberFilter(field_name="protein_g_100g", lookup_expr="gte")
+    protein_max = filters.NumberFilter(field_name="protein_g_100g", lookup_expr="lte")
+
+    carbs_min = filters.NumberFilter(field_name="carbs_g_100g", lookup_expr="gte")
+    carbs_max = filters.NumberFilter(field_name="carbs_g_100g", lookup_expr="lte")
+
+    fat_min = filters.NumberFilter(field_name="fat_g_100g", lookup_expr="gte")
+    fat_max = filters.NumberFilter(field_name="fat_g_100g", lookup_expr="lte")
+
+    class Meta:
+        model = FoodItem
+        fields = [
+            "food_type",
+            "vegan",
+            "vegetarian",
+            "pescetarian",
+            "gluten_free",
+            "lactose_free",
+        ]
